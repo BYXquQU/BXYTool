bb64b1ec6f3a331a9ce1ff4c6cfcf80a6f7ae458
saifing <saifing@saifingdemac-mini.local>
2016-08-01 11:44:04 +0800

    测试在GitHub提交自己的第一个项目

---
+++ BYXToolTest/BYXToolTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BYXToolTest
+//
+//  Created by saifing on 16/8/1.
+//  Copyright © 2016年 saifing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BYXToolTest/BYXToolTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BYXToolTest
+//
+//  Created by saifing on 16/8/1.
+//  Copyright © 2016年 saifing. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
